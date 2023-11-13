38fae44e4139c199f3bd5ac1e1eae71927562922
anejaanmol <105015233+anejaanmol@users.noreply.github.com>
2023-11-13 15:31:34 +0530

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,31 @@
+console.log(3+3)
+
+//alert(3+3)// we are using NODEjs // not browser js
+
+console.log("Anmol")
+
+let name="Anmol"
+let age=18
+let isLoggedIn=false
+let state;
+
+console.log([name,state,isLoggedIn,age]);
+
+// TYPES OF DATATYPES 
+
+
+// number => 2 to power of 53 
+// bigint => bada number hota h jab
+// string => ""
+// boolean=> true/false
+// null => standalone value
+//undefined => 
+//symbol => unique
+
+
+// object
+
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
+
