24db2c5c7182f16ede7d554375d1bbd68f7e85b8
anejaanmol <105015233+anejaanmol@users.noreply.github.com>
2023-11-14 14:37:13 +0530

    Add files via upload

---
+++ conversion_datatypes.js
@@ -0,0 +1,29 @@
+let score="anmol"
+
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber =Number (score)
+
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+// "33" => 33
+// "33abc" => NaN -> not a number -> type iska NUMBER hi h 
+// true=> 1 : false=> 0
+
+let isLoggedIn= "a"
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1=> true ; 0=> false
+// ""=> false ;
+// "anmol" => true
+
+
+let  someNumber= 33
+let stringNumber= String(someNumber)
+console.log( typeof stringNumber)
